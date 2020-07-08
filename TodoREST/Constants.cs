db6c9e9f9611b1f4eed3b5b7910e5aa96da38f3c
hugo <hugo.delacruz@tecsup.edu.pe>
2020-07-07 20:57:23 -0500

    first commit

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        public static string RestUrl = "http://192.168.0.106:3000/productos";
+    }
+}
---
+++ TodoREST/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TodoREST
+{
+	public class TodoItem
+	{
+		public string id { get; set; }
+
+		public string nombre { get; set; }
+
+		public string desck { get; set; }
+
+		public string precio { get; set; }
+	}
+}
